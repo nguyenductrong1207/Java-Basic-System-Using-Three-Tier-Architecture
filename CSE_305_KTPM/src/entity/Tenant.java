884dc6d6b14d78260212dc65e3a2dfd3ec4b46a7
kiet.luong <kiet.luong.cit20@eiu.edu.vn>
2023-11-11 15:29:23 +0700

    upload code from Kiet

--- CSE_305_KTPM/src/entity/Tenant.java
+++ CSE_305_KTPM/src/entity/Tenant.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.HashMap;
+
 public class Tenant extends User {
 
 	private int tenantID;
@@ -7,6 +9,7 @@ public class Tenant extends User {
 	private String tenantLastName;
 	private String tenantEmail;
 	private String tenantPassword;
+	private HashMap<Integer, RentalContract_Lease> rentConMap;
 
 	public Tenant(int tenantID, String tenantFirstName, String tenantLastName, String tenantEmail,
 			String tenantPassword) {
@@ -18,12 +21,49 @@ public class Tenant extends User {
 		this.tenantPassword = tenantPassword;
 	}
 
-	public void CreateRentalContract() {
+	public void CreateRentalContract(int renID, RentalContract_Lease renCon) {
+		rentConMap.put(renID, renCon);
 
 	}
 
-	public void TerminateRentalContract() {
+	public void TerminateRentalContract(int renID) {
+		rentConMap.remove(renID);
+	}
 
+	public int getTenantID() {
+		return tenantID;
+	}
+
+	public void setTenantID(int tenantID) {
+		this.tenantID = tenantID;
+	}
+
+	public String getTenantFirstName() {
+		return tenantFirstName;
+	}
+
+	public void setTenantFirstName(String tenantFirstName) {
+		this.tenantFirstName = tenantFirstName;
+	}
+
+	public String getTenantLastName() {
+		return tenantLastName;
+	}
+
+	public void setTenantLastName(String tenantLastName) {
+		this.tenantLastName = tenantLastName;
+	}
+
+	public String getTenantEmail() {
+		return tenantEmail;
+	}
+
+	public void setTenantEmail(String tenantEmail) {
+		this.tenantEmail = tenantEmail;
+	}
+
+	public void setTenantPassword(String tenantPassword) {
+		this.tenantPassword = tenantPassword;
 	}
 
 }
